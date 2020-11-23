cce80ef6a33e03a28d66de03e0757a0e9d602725
Emily Willow <emily.shapiera@gmail.com>
2020-11-23 15:27:02 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -8,7 +8,7 @@ end
 end
 # code your #valid_move? method here
 def valid_move?(board, index)
-  if position_taken && board(index).between? (0,8)
+  if position_taken && (index).between? (0,8)
     return (true)
 
   end
