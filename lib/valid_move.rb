d8ecc73c6586f932ef175bdb3a341c6007eff1a7
Emily Willow <emily.shapiera@gmail.com>
2020-11-23 15:26:32 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -16,13 +16,3 @@ def valid_move?(board, index)
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
-def position_taken?(board, index)
-
-if board[index] == " " or board[index] == "" or board[index] == (nil)
-  return (false)
-
-else
-  return (true)
-end
-
-end
