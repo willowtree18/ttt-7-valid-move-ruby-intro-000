b5882f1d4dcceda3c530c05cf21f60f77a115d1d
Emily Willow <emily.shapiera@gmail.com>
2020-11-23 15:10:50 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,5 +1,9 @@
 # code your #valid_move? method here
-
+def valid_move(board, index)
+  if board[index] > 8
+    return (false)
+  elsif board[index] < 0
+    return (false)
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, index)
