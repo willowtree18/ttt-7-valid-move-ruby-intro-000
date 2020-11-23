459e7437f45514ce1062d371537588b3fdb56b33
Emily Willow <emily.shapiera@gmail.com>
2020-11-23 15:13:13 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,16 +1,3 @@
-# code your #valid_move? method here
-def valid_move(board, index)
-  if board[index] > 8
-    return (false)
-  elsif board[index] < 0
-    return (false)
-  else
-    return (true)
-
-  end
-end
-
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, index)
 
 if board[index] == " " or board[index] == "" or board[index] == (nil)
@@ -21,3 +8,24 @@ else
 end
 
 end
+# code your #valid_move? method here
+def valid_move(board, index)
+  if board[index] > 8
+    return (false)
+  elsif board[index] < 0
+    return (false)
+  else
+    return (true)
+
+  end
+
+  if board[index] == " " or board[index] == "" or board[index] == (nil)
+    return (false)
+
+  else
+    return (true)
+  end
+  
+end
+
+# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
